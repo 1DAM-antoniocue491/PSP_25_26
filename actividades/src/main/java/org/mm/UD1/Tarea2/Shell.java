18047190d6e939d537d8f2679f6ab552eefb0cec
1DAM-antoniocue491 <acuelop369@g.educaand.es>
2025-09-25 13:32:31 +0200

    Update Shell.java

--- actividades/src/main/java/org/mm/UD1/Tarea2/Shell.java
+++ actividades/src/main/java/org/mm/UD1/Tarea2/Shell.java
@@ -1,7 +1,16 @@
 package org.mm.UD1.Tarea2;
 
+import java.util.Arrays;
+
 public class Shell {
+    private static final String[] OS = System.getProperty("os.name").split(" ");
     public static void main(String[] args) {
-        System.out.println(System.getProperty("os.name"));
+        if (Arrays.asList(OS).contains("Linux")) {
+            System.out.println("Es un sistema operativo linux");
+        } else if (Arrays.asList(OS).contains("Windows")) {
+            System.out.println("Es un sistema operativo Windows");
+        } else {
+            System.out.println("Es otro SO desconocido");
+        }
     }
 }
